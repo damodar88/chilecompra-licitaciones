a8b3882e8e4100966763edb8949e98113feca9fd
damodar88 <damodarstt@gmail.com>
2025-05-18 23:11:11 -0400

    first commit

---
+++ src/main/java/com/chilecompra_licitaciones/ChilecompraApplication.java
@@ -0,0 +1,13 @@
+package com.chilecompra_licitaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChilecompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChilecompraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chilecompra_licitaciones/ChilecompraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chilecompra_licitaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChilecompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
